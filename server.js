9e11908f47aee816f49e209d630ccd0dbd325cd1
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-06-27 15:17:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
