dc081eb62f1e0c4408ac6dd06ed37d8230f7f8b4
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-06-30 19:44:06 -0400

    Add server.js

--- server.js
+++ server.js
@@ -8,8 +8,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 mongoose.set('debug', true);
 
 app.use(require('./routes'));
 
-app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`You are now connect on localhost:${PORT}`));
